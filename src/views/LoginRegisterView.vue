e143b468d84d2299474152406adbcc75c3703afa
Jose Alb <josealberto_hm21@hotmail.com>
2023-04-04 20:58:00 +0200

    bootstrap y login v.00

--- src/views/LoginRegisterView.vue
+++ src/views/LoginRegisterView.vue
@@ -1,9 +1,44 @@
 <template>
-aaaaa
+<div>
+    <div class="row" style="width:50%; border-top:1px solid grey;border-left:1px solid grey;border-right:1px solid grey;background-color:beige; height:40px;margin-left: auto; margin-right: auto;">
+        <div id="tablogin" class="col-6" style="background-color: white;">Login</div>
+        <div id="tabregistro" class="col-6">Registro</div>
+    </div>
+    <div id="cuerpo" class=" w-50 p-4" style="margin-left: auto; margin-right: auto; border-left: 1px solid grey ;border-right: 1px solid grey ; border-bottom: 1px solid grey ;">
+        
+  <div class="row mt-4">
+    <div class="col-3"><label for="exampleDropdownFormEmail2">Email address</label></div>
+    <div class="col-9"><input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com"></div>
+  </div>
+
+
+  <div class="row mt-4">
+    <div class="col-3"><label for="exampleDropdownFormPassword2">Password</label></div>
+    <div class="col-9"><input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password"></div>
+  </div>
+
+
+  <div class="row mt-3">
+    
+    <div class="col-9"><label class="" for="dropdownCheck2">
+      Remember me
+    </label> <input type="checkbox" name="remember" id="remember"></div>
+    
+  </div>
+
+    <div class=" col-12 mt-3">
+        <button class="btn btn-primary w-50" style="margin-left:auto; margin-right: auto;">Sign in</button>
+    </div>
+
+    </div>
+
+
+</div>
 </template>
 
 <script lang="ts" setup>
 import {principal} from '../store/principal';
+
 
 const store = principal()
 store.guardatoken("lalalalalalalal")
